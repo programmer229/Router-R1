6bb25eb4789e650351264c7471664e068d035de0
programmer229 <ddengalev@gmail.com>
2025-10-06 13:40:52 +0200

    Fix bug

--- verl/workers/actor/dp_actor.py
+++ verl/workers/actor/dp_actor.py
@@ -110,8 +110,6 @@ class DataParallelPPOActor(BasePPOActor):
                                                                                                 sp_size=self.ulysses_sequence_parallel_size)
                     input_ids_rmpad_rolled, _, _ = ulysses_pad_and_slice_inputs(input_ids_rmpad_rolled, None,
                                                                                 self.ulysses_sequence_parallel_size)
-                position_ids_rmpad = position_ids_rmpad - 1
-
                 input_ids_rmpad_rolled = input_ids_rmpad_rolled.squeeze(0)  # ((total_nnz / sp) + pad)
 
                 # only pass input_ids and position_ids to enable flash_attn_varlen
